ae1ec32496145b1aa5e07965d8bede765c9d19fc
Titus de Maupeou <titus@esign.eu>
2019-12-20 15:28:47 +0100

    t

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import FetchData from "./FetchData";
+import FetchData from "../hooks/FetchData";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableHead from "@material-ui/core/TableHead";
@@ -8,6 +8,10 @@ import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import TablePagination from "@material-ui/core/TablePagination";
 import { Paper } from "@material-ui/core";
+import MaterialTable from 'material-table';
+import {Cell, Row} from 'material-table';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
 
 const API_URL =
   "https://app.ticketmaster.com/discovery/v2/events.json?apikey=3ofV0pnHEKQLOpEUzPvMmDkW2vzJOGJd";
@@ -26,6 +30,30 @@ const DataTable = () => {
     setPage(0);
   };
 
+  function TablePaginationActions(props) {
+    const { count, page, rowsPerPage, onFilterPrice } = props;
+  
+    const filterPrice = event => {
+      onFilterPrice(event, )
+    }
+
+    return (
+      <div className="test">
+        <IconButton
+          onClick={filterPrice}
+          >
+        </IconButton>
+      </div>
+    );
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
+
 // const priceRanges = Object(dataState.data).map((key, index) => {
 //   let arr = []
 //   dataState.data[index].priceRanges !== undefined
@@ -35,64 +63,70 @@ const DataTable = () => {
 //   return arr;
 // });
 
-const priceRanges = (row, index) => Object.values(row).map((key, index) => {
+const priceRanges = (row, priceMin) => Object.values(row).map((key, index) => {
   let arr = []
   row.priceRanges !== undefined
-  ? row.priceRanges.map(j =>
+  ? row.priceRanges
+  .map(j =>
       arr[index] = j.min + " to " + j.max + " " + j.currency
     ) : arr[index] = "-"
   return arr;
 });
 
-  return (
-    <div className="container">
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Event</TableCell>
-              <TableCell>Price Ranges</TableCell>
-              <TableCell>Date</TableCell>
+return (
+  <div className="container">
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Event</TableCell>
+            <TableCell>Price Ranges</TableCell>
+            <TableCell>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? dataState.data.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : dataState.data
+          ).map((row, i) => (
+            <TableRow key={row.id}>
+              <TableCell>
+                <Link to={"/" + row.id}>{row.name}</Link>
+              </TableCell>
+              <TableCell>
+                <Link to={"/" + row.id}>
+                {priceRanges(row, i)[0]}
+                </Link>
+              </TableCell>
+              <TableCell>
+                <Link to={"/" + row.id}>
+                  {row.dates.start.localDate} {row.dates.start.localTime}
+                </Link>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? dataState.data.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : dataState.data
-            ).map((row, i) => (
-              <TableRow key={row.id}>
-                <TableCell>
-                  <Link to={"/" + row.id}>{row.name}</Link>
-                </TableCell>
-                <TableCell>
-                  <Link to={"/" + row.id}>
-                  {priceRanges(row, i)[0]}
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <Link to={"/" + row.id}>
-                    {row.dates.start.localDate} {row.dates.start.localTime}
-                  </Link>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <TablePagination
-          rowsPerPageOptions={[5, 10]}
-          component="div"
-          count={50}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </div>
-  );
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10]}
+        component="div"
+        count={dataState.data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+        ActionsComponent={TablePaginationActions}
+      />
+    </Paper>
+    {/* <MaterialTable 
+      options={{
+        filtering:true
+      }}
+    /> */}
+  </div>
+);
 };
-
 export default DataTable;
